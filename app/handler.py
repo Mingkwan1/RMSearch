7035248295b3bb1dae5c375abc084638d799d0fa
Mingkwan Rattanasiwamok <mingkwan.ratta@gmail.com>
2025-04-29 11:50:38 +0800

    Added mounted network volume. Also added logging to handler and model_loader for easily debugging. Not calling the llm.py file for now

--- app/handler.py
+++ app/handler.py
@@ -1,10 +1,29 @@
-import runpod
-from llm import get_answer
+import runpod, logging
+from app.rmsearch.rmsearch import Search
+from app.model_loader import confirm_model_downloaded
+
+# Setup logging
+logging.basicConfig(
+    level=logging.INFO,  # default to INFO level
+    format="%(asctime)s [%(levelname)s] %(message)s",
+)
 
 queries = ["How to make LLM?", "What's the capital of Japan?"] * 5
 keys = ["LLM is Large Language Model which can be made ...", "Japanese capital is ..."] * 5
 
-print(">>> handler.py loaded ✅")
+logging.info(">>> handler.py loaded ✅")
+
+model_dir = confirm_model_downloaded()
+
+search = Search(model_name = model_dir,
+        tensor_parallel_size = 1,
+        pipeline_parallel_size = 2,)
+
+async def get_answer(queries, keys):
+    output = await search(queries, keys)
+    logging.info(output)
+    return output
+
 def handler(event):
     """
     This function processes incoming requests to your Serverless endpoint.
@@ -16,27 +35,27 @@ def handler(event):
         Any: The result to be returned to the client
     """
     
-    print(">>> handler.py called ✅")
+    logging.info(">>> handler.py called ✅")
     # Extract input data
-    print(f"Worker Start")
+    logging.info(f"Worker Start")
     try:
         input = event['input']
         
         prompt = input.get('prompt')  
         seconds = input.get('seconds', 0)  
 
-        print(f"Received prompt: {prompt}")
-        print(f"Sleeping for {seconds} seconds...")
+        logging.info(f"Received prompt: {prompt}")
+        logging.info(f"Sleeping for {seconds} seconds...")
         
         # You can replace this sleep call with your Python function to generate images, text, or run any machine learning workload
         result = get_answer(queries= prompt, keys = keys)
         return result 
     except Exception as e:
-        print(f"Error in handler: {str(e)}")
-        print(f"CRITICAL: {str(e)}", flush=True)  # Visible in RunPod logs
+        logging.info(f"Error in handler: {str(e)}")
+        logging.info(f"CRITICAL: {str(e)}", flush=True)  # Visible in RunPod logs
         raise # Ensure the error gets captured
     
 # Start the Serverless function when the script is run
 if __name__ == '__main__':
-    print("🔧 Starting serverless handler...")
+    logging.info("🔧 Starting serverless handler...")
     runpod.serverless.start({'handler': handler })
--- app/model_loader.py
+++ app/model_loader.py
@@ -1,13 +1,26 @@
-import os
+import os, time, logging
 from huggingface_hub import snapshot_download
 
-MODEL_DIR = "/models/llama3b-rm"
+# MODEL_DIR = "/models/llama3b-rm"
+MODEL_DIR = "/runpod-volume/models/llama3b-rm"
+
+# Setup logging
+logging.basicConfig(
+    level=logging.INFO,  # default to INFO level
+    format="%(asctime)s [%(levelname)s] %(message)s",
+)
 
 def confirm_model_downloaded():
     if not os.path.exists(MODEL_DIR):
-        print("🔵 Downloading model...")
-        snapshot_download(repo_id="Ray2333/GRM-Llama3.2-3B-rewardmodel-ft", local_dir=MODEL_DIR)
-        print("✅ Download complete.")
+        logging.info("Model not found. Starting download...")
+        os.makedirs(MODEL_DIR, exist_ok=True)
+        start_time = time.time()
+        snapshot_download(repo_id="Ray2333/GRM-Llama3.2-3B-rewardmodel-ft", local_dir=MODEL_DIR,
+                          local_dir_use_symlinks=False)
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        logging.info(f"Download complete. Time taken: {elapsed_time:.2f} seconds.")
     else:
-        print("🟢 Model already exists, skipping download.")
+        logging.info("Model already exists, skipping download.")
+        
     return MODEL_DIR
